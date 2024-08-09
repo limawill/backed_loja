babbe3130e9886d52ae43e81313208297b944317
limawill <limawill83@gmail.com>
2024-08-09 13:19:58 -0300

    Finalizando testes das classes

--- README.md
+++ README.md
@@ -1,2 +1,38 @@
-# backed_loja
-desafio
+# Desafio de Solução Tech - BHub
+
+Este projeto consiste no desenvolvimento de um sistema de processamento de pedidos para uma grande empresa, enfrentando o desafio de centralizar e automatizar um conjunto complexo e em constante evolução de regras de negócio. O sistema visa consolidar as diversas práticas comerciais que, até então, eram gerenciadas de maneira dispersa, com uma combinação de processos manuais e automatizados. Pela descrição do documento dar a entender que é utilizado uma mistura de práticas comerciais manuais e automatizadas, dependendo muitas vezes conhecimento de funcionários, que conhecem regras específicas não documentadas.
+
+## Objetivo
+
+O principal objetivo deste projeto é criar um sistema robusto e flexível que automatize o processamento de pedidos, incluindo a geração de guias de remessa, ativações de associações, e pagamentos de comissões, entre outros. O sistema deve ser capaz de gerenciar de forma eficiente as regras de negócio complexas e arbitrárias, permitindo ajustes rápidos para acomodar mudanças e exceções. Para abordar esses desafios, foi desenvolvido um sistema que integra todas as regras de negócio identificadas, automatizando processos anteriormente geridos de forma ad-hoc. A solução inclui a capacidade de gerar guias de remessa, processar associações e upgrades, enviar notificações por e-mail, e calcular comissões automaticamente, conforme ilustrado no fluxograma abaixo:
+
+<img src="/imagens/fluxograma.jpg">
+
+Com o objetivo de unificar essas práticas em uma única aplicação, foi necessário projetar um sistema que pudesse acomodar a diversidade das regras de negócio, sem sacrificar a flexibilidade ou a eficiência.
+
+## Tecnologias Usadas
+
+| Tecnologia     | Descrição                                                                              |
+| -------------- | -------------------------------------------------------------------------------------- |
+| Python         | [pLinguagem de programação utilizada para implementar o serviço                        |
+| FastAPI        | Framework para criar APIs web assíncronas e de alto desempenho                         |
+| Redis          | Banco de dados em memória para gerenciamento de mensagens e streams                    |
+| Pydantic       | Biblioteca para validação de dados e criação de modelos de dados                       |
+| Docker         | Plataforma que permite a criação, implantação e execução de aplicativos em contêineres |
+| Docker Compose | Ferramenta que facilita a definição e execução de aplicativos multi-contêineres Docker |
+| PostgreSQL     | Sistema de gerenciamento de banco de dados relacional robusto e open-source            |
+
+## Instalação
+
+Com o seu sistema precisa ter o git e docker já instalado. Com os mesmo configurados e rodando faça o clone do projeto:
+
+```sh
+git clone git@github.com:limawill/backed_loja.git
+```
+
+Dentro do projeto crie seu Virtual Environment (venv) e os ative (comandos abaixo para linux/mac)
+
+```sh
+python -m venv .venv
+source .venv/bin/activate
+```
--- tests/produto_fisico/test_processar_compra.py
+++ tests/produto_fisico/test_processar_compra.py
@@ -40,8 +40,6 @@ class TestesProdutosFisicos:
 
         dict_data = json.loads(json_data)
         df = pd.json_normalize(dict_data)
-        print(df)
-        print(df.columns)
         return df
 
     @pytest.fixture
@@ -67,10 +65,8 @@ class TestesProdutosFisicos:
             }
         }
         '''
-
         dict_data = json.loads(json_data)
-        df = pd.DataFrame([dict_data])
-        df = pd.json_normalize(df)
+        df = pd.json_normalize(dict_data)
         return df
 
     @pytest.mark.asyncio
@@ -93,3 +89,142 @@ class TestesProdutosFisicos:
             resultado = await compra_fisica.insere_venda_livro(df_entrada_livro)
             assert resultado is None
             mock_executa_insert.assert_called_once()
+
+    @pytest.mark.asyncio
+    async def test_insere_comissao_sucesso(self, compra_fisica, df_entrada_livro):
+
+        with patch.object(compra_fisica.db_connection, 'connect', new_callable=AsyncMock), \
+                patch.object(compra_fisica.db_connection, 'close', new_callable=AsyncMock), \
+                patch.object(compra_fisica.db_connection, 'executa_insercao_retorna_id', new_callable=AsyncMock) as mock_executa_insert:
+            mock_executa_insert.return_value = 7
+            resultado = await compra_fisica.insere_venda_comissao(df_entrada_livro, 7)
+        assert resultado == 7
+        mock_executa_insert.assert_called_once()
+
+    @pytest.mark.asyncio
+    async def test_insere_comissao_none(self, compra_fisica, df_entrada_livro):
+
+        with patch.object(compra_fisica.db_connection, 'connect', new_callable=AsyncMock), \
+                patch.object(compra_fisica.db_connection, 'close', new_callable=AsyncMock), \
+                patch.object(compra_fisica.db_connection, 'executa_insercao_retorna_id', new_callable=AsyncMock) as mock_executa_insert:
+            mock_executa_insert.return_value = None
+            resultado = await compra_fisica.insere_venda_comissao(df_entrada_livro, 7)
+        assert resultado is None
+        mock_executa_insert.assert_called_once()
+
+    @pytest.mark.asyncio
+    async def test_insere_royalty_remessa_livro_sucesso(self, compra_fisica, df_entrada_livro):
+
+        with patch.object(compra_fisica.db_connection, 'connect', new_callable=AsyncMock), \
+                patch.object(compra_fisica.db_connection, 'close', new_callable=AsyncMock), \
+                patch.object(compra_fisica.db_connection, 'executa_insercao_retorna_id', new_callable=AsyncMock) as mock_executa_insert:
+            mock_executa_insert.return_value = 7
+            resultado = await compra_fisica.insere_venda_royalty_remessa(df_entrada_livro, 7)
+        assert resultado == 7
+        mock_executa_insert.assert_called_once()
+
+    @pytest.mark.asyncio
+    async def test_insere_royalty_remessa_livro_none(self, compra_fisica, df_entrada_livro):
+
+        with patch.object(compra_fisica.db_connection, 'connect', new_callable=AsyncMock), \
+                patch.object(compra_fisica.db_connection, 'close', new_callable=AsyncMock), \
+                patch.object(compra_fisica.db_connection, 'executa_insercao_retorna_id', new_callable=AsyncMock) as mock_executa_insert:
+            mock_executa_insert.return_value = None
+            resultado = await compra_fisica.insere_venda_royalty_remessa(df_entrada_livro, 7)
+        assert resultado is None
+        mock_executa_insert.assert_called_once()
+
+    @pytest.mark.asyncio
+    async def test_insere_royalty_remessa_produto_sucesso(self, compra_fisica, df_entrada_produto):
+        with patch.object(compra_fisica.db_connection, 'connect', new_callable=AsyncMock), \
+                patch.object(compra_fisica.db_connection, 'close', new_callable=AsyncMock), \
+                patch.object(compra_fisica.db_connection, 'executa_insercao_retorna_id', new_callable=AsyncMock) as mock_executa_insert:
+            mock_executa_insert.return_value = 7
+            resultado = await compra_fisica.insere_venda_royalty_remessa(df_entrada_produto, 7)
+        assert resultado == 7
+        mock_executa_insert.assert_called_once()
+
+    @pytest.mark.asyncio
+    async def test_insere_royalty_remessa_produto_none(self, compra_fisica, df_entrada_produto):
+        with patch.object(compra_fisica.db_connection, 'connect', new_callable=AsyncMock), \
+                patch.object(compra_fisica.db_connection, 'close', new_callable=AsyncMock), \
+                patch.object(compra_fisica.db_connection, 'executa_insercao_retorna_id', new_callable=AsyncMock) as mock_executa_insert:
+            mock_executa_insert.return_value = None
+            resultado = await compra_fisica.insere_venda_royalty_remessa(df_entrada_produto, 7)
+        assert resultado is None
+        mock_executa_insert.assert_called_once()
+
+    @pytest.mark.asyncio
+    async def test_insere_dados_banco_sucesso(self, compra_fisica):
+        db_connection_mock = AsyncMock()
+        db_connection_mock.executa_insercao_retorna_id.return_value = 1
+        session_mock = AsyncMock()
+        db_connection_mock.session = session_mock
+        compra_fisica.db_connection = db_connection_mock
+        df = pd.DataFrame({'coluna1': [1], 'coluna2': [2]})
+        queries = "INSERT INTO tabela (coluna1, coluna2) VALUES (:coluna1, :coluna2)"
+        parameters = {'coluna1': 1, 'coluna2': 2}
+        resultado = await compra_fisica.insere_dados_banco(df, queries, parameters)
+        assert resultado == 1
+        db_connection_mock.executa_insercao_retorna_id.assert_called_once_with(
+            session_mock, queries, df, parameters
+        )
+
+    @pytest.mark.asyncio
+    async def test_insere_dados_banco_none(self, compra_fisica):
+        db_connection_mock = AsyncMock()
+        db_connection_mock.executa_insercao_retorna_id.return_value = None
+        session_mock = AsyncMock()
+        db_connection_mock.session = session_mock
+        compra_fisica.db_connection = db_connection_mock
+        df = pd.DataFrame({'coluna1': [1], 'coluna2': [2]})
+        queries = "INSERT INTO tabela (coluna1, coluna2) VALUES (:coluna1, :coluna2)"
+        parameters = {'coluna1': 1, 'coluna2': 2}
+        resultado = await compra_fisica.insere_dados_banco(df, queries, parameters)
+        assert resultado is None
+        db_connection_mock.executa_insercao_retorna_id.assert_called_once_with(
+            session_mock, queries, df, parameters
+        )
+
+    @pytest.mark.asyncio
+    async def test_insere_dados_banco_excecao(self, compra_fisica):
+        db_connection_mock = AsyncMock()
+        db_connection_mock.executa_insercao_retorna_id.side_effect = Exception(
+            "Erro ao inserir no banco de dados")
+        session_mock = AsyncMock()
+        db_connection_mock.session = session_mock
+        compra_fisica.db_connection = db_connection_mock
+        df = pd.DataFrame({'coluna1': [1], 'coluna2': [2]})
+        queries = "INSERT INTO tabela (coluna1, coluna2) VALUES (:coluna1, :coluna2)"
+        parameters = {'coluna1': 1, 'coluna2': 2}
+        with pytest.raises(Exception) as exc_info:
+            await compra_fisica.insere_dados_banco(df, queries, parameters)
+        assert str(exc_info.value) == "Erro ao inserir no banco de dados"
+        db_connection_mock.executa_insercao_retorna_id.assert_called_once_with(
+            session_mock, queries, df, parameters
+        )
+
+    @pytest.mark.asyncio
+    async def test_process_message_sucesso(self, compra_fisica):
+        # Mockando os métodos insere_venda_livro, insere_venda_comissao e insere_venda_royalty_remessa
+        compra_fisica.insere_venda_livro = AsyncMock(return_value=1)
+        compra_fisica.insere_venda_comissao = AsyncMock(return_value=1)
+        compra_fisica.insere_venda_royalty_remessa = AsyncMock(return_value=1)
+
+        # Mockando o Redis
+        redis_mock = MagicMock()
+        compra_fisica.r = redis_mock
+
+        message_data = {
+            b'data': b'{"coluna1": 1, "coluna2": 2}'
+        }
+        message = ('stream_app1_app3', [(b'msg_id_1', message_data)])
+        await compra_fisica.process_message(message)
+        compra_fisica.insere_venda_livro.assert_called_once()
+        compra_fisica.insere_venda_comissao.assert_called_once()
+
+        compra_fisica.insere_venda_royalty_remessa.assert_called_once()
+        redis_mock.xadd.assert_called_once_with(
+            'stream_app3_app1', {'status': 'true', 'venda_id': '1'}
+        )
+        assert compra_fisica.last_id == b'msg_id_1'
